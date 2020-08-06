f7b4fdd6fa20db9961319be1527bc835628ac96e
Jake Almeda <jakealmeda@gmail.com>
2020-08-06 20:25:13 +0800

    Paypal Shortcode Added

--- setup-plugin-repo.php
+++ setup-plugin-repo.php
@@ -39,10 +39,28 @@ function setup_return_site_url() {
 
 
 /* --------------------------------------------------------------------------------------------
- * THIS SHORTCODE SIMPLY RETURNS CURRENT THEME'S (CHILD) URL
- * MAINLY USED FOR THE PIXEL.PNG FOUND IN PAYPAL BUTTONS
+ * DONATE BUTTON
  * ----------------------------------------------------------------------------------------- */
-add_shortcode( 'setup-current-theme-url', 'setup_current_theme_url' );
-function setup_current_theme_url() {
-	return get_stylesheet_directory_uri().'/';
+add_shortcode( 'setup_paypal_donate_button', 'setup_paypal_donate_button_func' );
+function setup_paypal_donate_button_func() {
+	return '<input type="image" src="'.get_stylesheet_directory_uri().'/assets/images/pixel.png" class="space-bottom-half cta-donate-paypalbutton aligncenter" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">';
 }
+
+
+/* --------------------------------------------------------------------------------------------
+ * BUY BUTTON (Phone Coaching - 1st button)
+ * ----------------------------------------------------------------------------------------- */
+add_shortcode( 'setup_paypal_sbcp', 'setup_paypal_sbcp_func' );
+function setup_paypal_sbcp_func() {	
+	return '<input onclick="PC_PhoneCoaching()" alt="PayPal - The safer, easier way to pay online!" name="submit" class="space-bottom-half cta-paypal" src="'.get_stylesheet_directory_uri().'/assets/images/pixel.png" type="image">';
+}
+
+
+/* --------------------------------------------------------------------------------------------
+ * BUY BUTTON (Phone Coaching - 2nd button)
+ * ----------------------------------------------------------------------------------------- */
+add_shortcode( 'setup_paypal_sbcp_2', 'setup_paypal_sbcp_2_func' );
+function setup_paypal_sbcp_2_func() {
+	return '<input onclick="PC_PhoneCoaching()" alt="PayPal - The safer, easier way to pay online!" name="submit" class="space-bottom-half cta-buy-paypalbutton aligncenter" src="'.get_stylesheet_directory_uri().'/assets/images/pixel.png" type="image">';
+}
+
