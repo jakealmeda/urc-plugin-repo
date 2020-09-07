84288a377dbe74de15c1913cb234f2e66e296880
Jake Almeda <jakealmeda@gmail.com>
2020-09-08 00:09:44 +0800

    Dequeued all JavaScript

--- codes/setup-mobile-front.php
+++ codes/setup-mobile-front.php
@@ -15,4 +15,16 @@ if( $detect->isMobile() ) {
     //add_action( 'genesis_before_content_sidebar_wrap', 'setup_show_in_mobile' ); 
     add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
+    // list JS handlers
+    add_action( 'wp_print_scripts', 'wsds_detect_enqueued_scripts' );
+    add_action( 'wp_footer', 'wsds_detect_enqueued_scripts' );
+}
+
+
+function wsds_detect_enqueued_scripts() {
+    global $wp_scripts;
+    foreach( $wp_scripts->queue as $handle ) :
+        //echo $handle . ' | ';
+        setup_check_enqueued_javascript( $handle );
+    endforeach;
 }
