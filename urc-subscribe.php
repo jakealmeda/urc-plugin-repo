18cdf661a31988a0dbbc23eedd5135c706f0ca98
Mark Corpuz <social@markcorpuz.com>
2020-12-07 09:36:31 -0800

    Made some link changes to CTA elements

--- urc-subscribe.php
+++ urc-subscribe.php
@@ -33,6 +33,7 @@ function urc_subscribe_function() {
 	//$free_ebook_page = get_permalink( get_page_by_path( "free-ebook" ) );
 	//$free_ebook_page = setup_free_ebook_daw();
 	$products_page = get_permalink( get_page_by_path( "products" ) );
+	$phone_coaching_page = get_permalink( get_page_by_path( "phone-coaching" ) );
 
 	global $post; //$post->ID
 	if( in_array( $post->post_name, urc_hide_subsribe_from_the_following_pages() ) ) {
@@ -57,20 +58,20 @@ function urc_subscribe_function() {
 	$content = $hide_on_these_pages.$cta_ebook_expanded.'<div class="group grid-cta-icon">
 			'.$cta_ebook_compressed.'
 			<div class="module cta-icon"><div class="module-wrap">
-				<div><a class="item image link" href="'.$products_page.'" data-type="page"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-coaching-icon.png" alt="" class="wp-image-41163" width="50" height="50"></a></div>
-				<div class="items info"><div><a class="item title link" href="'.$products_page.'" data-type="page" data-id="1519">Phone/Skype Coaching Session</a></div></div>
+				<div><a class="item image link" href="'.$phone_coaching_page.'" data-type="page"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-coaching-icon.png" alt="" class="wp-image-41163" width="50" height="50"></a></div>
+				<div class="items info"><div><a class="item title link" href="'.$phone_coaching_page.'" data-type="page" data-id="1519">Phone/Skype Coaching Session</a></div></div>
 			</div></div>
 			<div class="module cta-icon"><div class="module-wrap">
 				<div><a class="item image link" href="https://teespring.com/stores/coach-corey-wayne"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-products-icon.png" alt="" class="wp-image-41161" width="50" height="50"></a></div>
 				<div class="items info"><div><a class="item title link" href="https://teespring.com/stores/coach-corey-wayne">Coach Corey Wayne Merchandise</a></div></div>
 			</div></div>
 			<div class="module cta-icon"><div class="module-wrap">
-				<div><a class="item image link" href="'.$donate_page.'" data-type="page" ><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-donate-icon.png" alt="" class="width="50" height="50"></a></div>
-				<div class="items info"><div><a class="item title link" href="'.$donate_page.'" data-type="page">Support My Work</a></div></div>
+				<div><a class="item image link" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LKGTSSLYJ93J6" data-type="page" ><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-donate-icon.png" alt="" class="width="50" height="50"></a></div>
+				<div class="items info"><div><a class="item title link" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LKGTSSLYJ93J6">Make A Donation</a></div></div>
 			</div></div>
 			<div class="module cta-icon"><div class="module-wrap">
 				<div><a class="item image link" href="'.$products_page.'" data-type="page"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-all-books-icon.png" alt="" class="wp-image-41162" width="50" height="50"></a></div>
-				<div class="items info"><div><a class="item title link" href="'.$products_page.'" data-type="page">Products, Coaching, Books & More</a></div></div>
+				<div class="items info"><div><a class="item title link" href="'.$products_page.'" data-type="page">Books & Recommended Products</a></div></div>
 			</div></div>
 		</div>';
 
@@ -114,7 +115,7 @@ function setup_cta_compressed() {
 
 	return '<div class="module cta-icon" id="cta_compressed_target"><div class="module-wrap">
 				<div><a class="item image link" href="'.$free_ebook_page.'" data-type="page"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-free-ebook-icon.png" alt="" class="wp-image-41165" width="50" height="50"></a></div>
-				<div class="items info"><div><a class="item title link" href="'.$free_ebook_page.'" data-type="page" data-id="1519">Free eBook & Audio Program</a></div></div>
+				<div class="items info"><div><a class="item title link" href="'.$free_ebook_page.'" data-type="page" data-id="1519">Free eBooks & Audio Program</a></div></div>
 			</div></div>';
 }
 
