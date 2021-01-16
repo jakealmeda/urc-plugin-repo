a51bae1dd9212bc39e023afeba84b4e59c183a9e
Jake Almeda <jakealmeda@users.noreply.github.com>
2021-01-16 13:58:44 +0800

    Update asset-desktab.js
    Implemented jQuery to handle the mobile version.

--- js/asset-desktab.js
+++ js/asset-desktab.js
@@ -57,35 +57,32 @@
 		
 	    if( ScreenSizer < 768 ) {
 
-        	//$( '.sidebar-primary' ).hide();
-        	if( CTApage != 'free-ebook' ) {
+        	/*if( CTApage != 'free-ebook' ) {
+        		// Not Free eBook
 
 	        	$( '#cta_compressed_target' ).hide();
 	        	
 	        	//if( CTApage != 'free-ebook' ) {
 	        	if( hideCTA != 2 ) {
-	        		$( '#cta_expander_target' ).html( CTAexpanded );
+	        		$( '#cta_expander_target' ).html( CTAfreeEbookIcons );
 	        	} else {
 	        		$( '#cta_expander_target' ).html( '' );
 	        	}
 
-	        } else {
+	        } else {*/
+	        if( CTApage == 'free-ebook' ) {
+	        	// Free-eBook page
+        		$( '#cta_expander_target' ).html( CTAexpanded );
 
-	        	// Free-eBook page | 615
-	        	//if( ScreenSizer <= 700 ) {
-
-	        		//$( '#cta_compressed_target' ).show();
-
-	        		$( '#cta_expander_target' ).html( CTAexpanded );
-
-	        	//}
 	        }
+
+	        desktab_show_hide( 'hide' );
 
 	    } else {
 
         	//$( '.sidebar-primary' ).show();
 
-    		$( '#cta_compressed_target' ).show();
+    		//$( '#cta_compressed_target' ).show();
 
     		if( CTApage == 'free-ebook' ) {
     			$( '#cta_expander_target' ).html( CTAfreeEbookIcons );
@@ -93,7 +90,30 @@
         		$( '#cta_expander_target' ).html( '' );
         	}
 
+        	desktab_show_hide( 'show' );
+
 	    }
+
+	}
+
+	function desktab_show_hide( ShowHide ) {
+
+		if( ShowHide == 'hide' ) {
+
+			// check first if element is visible
+			if( $( '.sidebar-primary' ).is(":visible") ) {
+				// hide
+				$( '.sidebar-primary' ).hide();
+			}
+
+		} else {
+
+			if( $( '.sidebar-primary' ).is(":hidden") ) {
+				// hide
+				$( '.sidebar-primary' ).show();
+			}
+
+		}
 
 	}
 
